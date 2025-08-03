9a3b998e006c9c05c6b72b198bcc04c4dccf67e7
stella659 <stella.koivula@helsinki.fi>
2025-08-03 20:39:07 +0300

    Show item list/info

--- items.py
+++ items.py
@@ -4,3 +4,18 @@ def add_item(title, description, duration, intensity, user_id):
     sql = """INSERT INTO items (title, description, duration, intensity, user_id)
             VALUES (?, ?, ?, ?, ?)"""
     db.execute(sql,[title, description, duration, intensity, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.description,
+                    items.duration,
+                    items.intensity,
+                    users.username
+            FROM items, users
+            WHERE items.user_id = users.id AND
+                items.id = ?"""
+    return db.query(sql, [item_id])[0]
